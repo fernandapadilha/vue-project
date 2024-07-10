62538ecd4a494fd1830d805e07ffc73ca410a196
fernandapadilha <vitoriafernandaifc@gmail.com>
2024-07-10 11:54:19 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue';
+
+</script>
+
+<template>
+  <ExpandBox title="Caixa 1" />
+  <ExpandBox title="Caixa 2"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ExpandBox.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, computed, defineProps } from 'vue';
+
+const props = defineProps(['title']);
+const showContent = ref(false);
+const textBotao = computed(
+    () => showContent.value ? 'Esconder' : 'Mostrar'
+)
+
+</script>
+
+<template>
+
+    <div>
+        <button @click="showContent = !showContent">{{ textBotao }}</button>
+        <div v-if="showContent" class="expand-box">
+            <h1>{{ props.title }}</h1>
+            <p>Texto da caixa</p>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+    .expand-box {
+        height: 100%;
+        padding: 20px;
+        border: 1px solid black;
+        border-radius: 5px;
+        box-shadow: 3px 3px 2px 2px gray;
+    }
+</style>
